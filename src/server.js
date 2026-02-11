d7a06436ea41351169282cdc54ca4dd4132c8fd1
Yunus Celik <yunusnotfound@icloud.com>
2026-02-11 11:32:00 +0300

    auth route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const helmet = require('helmet');
 require("dotenv").config();
 const { sequelize } = require('./models');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.use('/api/auth', authRoutes)
 
 app.get("/health", (req, res) => {
   res.status(200).json({
